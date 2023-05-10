edb04974796675da9a805bb501e7000192d238ee
Marcus <30908995+mickeystix@users.noreply.github.com>
2023-05-10 12:32:02 -0500

    Update config.lua removed unused variable

--- config.lua
+++ config.lua
@@ -2,4 +2,3 @@ Config = Config or {}
 
 Config.UseNoName = true -- Dont use any name when someone wins
 Config.UseCharName = false -- Uses the character name if true, if false it uses the steam / default name.
-Config.UsePlayerName = false -- Whatever identifier you use aka steamid, fivem, etc
